51321aa27eaa73019457f507cc9d1c37e8cbda36
Konstantin <tenenbaum1994@gmail.com>
2026-01-25 10:47:22 +0700

    реализован stat-client

---
+++ stat-service/stat-client/src/main/java/ru/practicum/client/config/StatClientConfig.java
@@ -0,0 +1,21 @@
+package ru.practicum.client.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+
+@Configuration
+public class StatClientConfig {
+
+    @Value("${stat-server.url}")
+    private String statServerUrl;
+
+    @Bean
+    public RestClient restClient() {
+        return RestClient.builder()
+                .baseUrl(statServerUrl)
+                .defaultHeader("Content-Type", "application/json")
+                .build();
+    }
+}
