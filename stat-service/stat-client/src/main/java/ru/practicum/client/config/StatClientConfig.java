fc7ba8b434e771ff4d4d71c400997c839e52d896
Konstantin <tenenbaum1994@gmail.com>
2026-01-25 11:00:48 +0700

    доработка StatClientConfig

--- stat-service/stat-client/src/main/java/ru/practicum/client/config/StatClientConfig.java
+++ stat-service/stat-client/src/main/java/ru/practicum/client/config/StatClientConfig.java
@@ -3,19 +3,19 @@ package ru.practicum.client.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.client.RestClient;
 
 @Configuration
 public class StatClientConfig {
 
-    @Value("${stat-server.url}")
-    private String statServerUrl;
-
     @Bean
-    public RestClient restClient() {
+    public RestClient restClient(@Value("${stat-server.url}") String statServerUrl) {
+
         return RestClient.builder()
                 .baseUrl(statServerUrl)
-                .defaultHeader("Content-Type", "application/json")
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .build();
     }
 }
