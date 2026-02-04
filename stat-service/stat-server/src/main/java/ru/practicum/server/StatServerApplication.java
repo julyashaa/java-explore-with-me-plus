f2c051ed682bec8d453be9715e75860d428cb406
Sergey <we_ew_2015@mail.ru.ru>
2026-02-04 22:54:26 +0300

    исправление юнит тестов №2

--- stat-service/stat-server/src/main/java/ru/practicum/server/StatServerApplication.java
+++ stat-service/stat-server/src/main/java/ru/practicum/server/StatServerApplication.java
@@ -3,7 +3,7 @@ package ru.practicum.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"ru.practicum.server", "ru.practicum.exception"})
+@SpringBootApplication
 public class StatServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(StatServerApplication.class, args);
