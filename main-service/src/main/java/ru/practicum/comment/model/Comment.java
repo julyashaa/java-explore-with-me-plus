bb302e02afb95cadd4829121ef48f4f9ee1637bf
Konstantin <tenenbaum1994@gmail.com>
2026-02-11 09:54:30 +0700

    добавлена таблица comments и сущность для этой таблицы - Comment

---
+++ main-service/src/main/java/ru/practicum/comment/model/Comment.java
@@ -0,0 +1,38 @@
+package ru.practicum.comment.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import ru.practicum.event.model.Event;
+import ru.practicum.user.model.User;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "comments")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "text", nullable = false)
+    private String text;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id", nullable = false)
+    private User author;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "event_id", nullable = false)
+    private Event event;
+
+    @Column(name = "created_on", nullable = false)
+    private LocalDateTime createdOn;
+
+    @Column(name = "edited_on")
+    private LocalDateTime editedOn;
+}
